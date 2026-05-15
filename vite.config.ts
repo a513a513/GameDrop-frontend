04fffd94e18f215670739c08abf854ed4cec54ee
a513a513 <51474310+a513a513@users.noreply.github.com>
2026-05-15 22:45:17 +0900

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "@lovable.dev/vite-tanstack-config";
-
-export default defineConfig({
-  // 러버블의 기본 설정을 무시하고 Vercel용으로 강제 지정
-  server: {
-    preset: 'vercel'
-  }
-});
